f91afd792365ca20d7a047ec7f4c9beb581b5f3a
Syed Mustajab Abbas Nqvi <85496904+wawawasi@users.noreply.github.com>
2022-08-04 12:35:37 +0500

    responsive index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,88 @@
-//     $(document).ready(function(){
+const menuNavbar = document.querySelector('.navbar');
+const menuInner = menuNavbar.querySelector('.menu-inner');
+const menuArrow = menuNavbar.querySelector('.menu-arrow');
+const menuBurger = document.querySelector('.burger');
+const closeNav = document.querySelector('.close-nav');
+const overlay = document.querySelector('.overlay');
+const search = document.querySelector('.search-bar');
+const searchIcon = document.querySelector('.search-icon');
+const closeSearchbar = document.querySelector('.close-search');
 
-         
-//         const minute = 1000 * 60,
-//         hour = minute * 60,
-//         day = hour * 24;
 
-//   let today = new Date(),
-//       dd = String(today.getDate()).padStart(2, "0"),
-//       mm = String(today.getMonth() + 1).padStart(2, "0"),
-//       yyyy = today.getFullYear(),
-//       nextYear = yyyy + 1,
-//       dayMonth = "07/30/",
-//       birthday = dayMonth + yyyy;
-  
 
-  
-//   const countDown = new Date(birthday).getTime(),
-//       x = setInterval(function() {    
 
-//         const now = new Date().getTime(),
-//           distance = countDown - now;
 
-//             document.getElementById("days").innerText = Math.floor(distance / (day)),
-//           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-//           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute))
+// Show Hide Navbar Menu Function
+const toggleMenu = () => {
+	menuNavbar.classList.toggle('active');
+	overlay.classList.toggle('active');
+	document.querySelector('body').style.overflow="hidden"
+};
 
-    
-//       }, 0)
+const closeMenu = () => {
+	menuNavbar.classList.remove('active');
+	overlay.classList.remove('active');
+	document.querySelector('body').style.overflow="auto"
+};
 
-//     });
-      
-      
+
+// Hide Mobile Submenu Function
+const hideSubMenu = () => {
+	subMenu.style.animation = 'slideRight 0.5s ease forwards';
+	setTimeout(() => {
+		subMenu.classList.remove('active');
+	}, 300);
+
+	menuNavbar.querySelector('.menu-title').textContent = '';
+	menuNavbar.querySelector('.menu-header').classList.remove('active');
+};
+
+// Show Mobile Submenu Function
+const showSubMenu = (children) => {
+	subMenu = children.querySelector('.submenu');
+	subMenu.classList.add('active');
+	subMenu.style.animation = 'slideLeft 0.5s ease forwards';
+	const menuTitle = children.querySelector('i').parentNode.childNodes[0].textContent;
+	menuNavbar.querySelector('.menu-title').textContent = menuTitle;
+	menuNavbar.querySelector('.menu-header').classList.add('active');
+};
+
+// Toggle Mobile Submenu Function
+const toggleSubMenu = (e) => {
+	if (!menuNavbar.classList.contains('active')) {
+		return;
+	}
+	if (e.target.closest('.menu-dropdown')) {
+		const children = e.target.closest('.menu-dropdown');
+		showSubMenu(children);
+	}
+};
+
+// Fixed Resize Window Function
+const resizeWindow = () => {
+	if (window.innerWidth > 991) {
+		if (menuNavbar.classList.contains('active')) {
+			toggleMenu();
+		}
+	}
+};
+
+// Initialize Event Listeners
+menuBurger.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', toggleMenu);
+menuArrow.addEventListener('click', hideSubMenu);
+menuInner.addEventListener('click', toggleSubMenu);
+closeNav.addEventListener('click', closeMenu);
+window.addEventListener('resize', resizeWindow);
+
+
+const openSearch = () => {
+	search.classList.toggle('active');
+};
+
+const closeSearch = () => {
+	search.classList.remove('active');
+};
+
+searchIcon.addEventListener('click', openSearch);
+closeSearchbar.addEventListener('click', closeSearch);
